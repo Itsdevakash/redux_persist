943e56e830b22ed46e5f1435760db4320af531c1
Itsdevakash <mauryaakash342@gmail.com>
2025-12-25 00:00:00 +0530

    in complete

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='bg-rose-600 flex items-center justify-center h-screen'>       
+  
+
+
+
+
+    </div>
+  )
+}
